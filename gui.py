6a4b9e670707436d30137957fa98db1093026964
zzachjoness <zzachjoness@gmail.com>
2023-01-19 14:14:45 -0500

    grid update

--- gui.py
+++ gui.py
@@ -16,26 +16,22 @@ def mk_board(containter, cell):
     frame.columnconfigure(2, weight=1)
     frame.grid(column=cell % 3, row=cell//3)
     for i in range(9):
-        field = ttk.Label(frame, text=cell*i+cell+i, width=5)
+        field = ttk.Label(frame, background='#0c3800', text=i+1, width=1,padding=10, border=1,relief='sunken')
+
         field.grid(column=i % 3, row=i//3)
 
+
 # Function for creating grid of Frames
-
-
 def mk_grid(container):
     for i in range(9):
         mk_board(container, i)
-
 
 # root window
 root = tk.Tk()  # creates and application windon
 root.geometry('400x400')
 root.resizable(True, True)
 root.title('Sudoku')
-# set root grid
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=1)
-root.columnconfigure(2, weight=1)
+
 
 mk_grid(root)
 
