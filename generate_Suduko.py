14cc943779438dfa75f043e670b3584229c46a49
zzachjoness <zzachjoness@gmail.com>
2023-01-21 22:41:41 -0500

    get_row sb_row fixes

--- generate_Suduko.py
+++ generate_Suduko.py
@@ -8,32 +8,39 @@ class Board():
     self.data = []
     self.msg = 'hi'
 
-  def get_row(self, sub_block,row):
+  def get_row(self, sub_block,fc):
     board_col = sub_block % 3
+    sb_row = fc // 3 # sub_block row based upon position of fixed cell
     if board_col == 0:
+      print('data check:', self.data)
+      print('should be done, here is your row list: ', [])
       return []
     row_list = []
-    sb_row = (row // 3) * 3
     for i in range(sub_block - board_col, sub_block):
       if len(self.data) >= i:
-        for j in range(sb_row, sb_row + 3):
-          row_list.append(self.data[i][j])
+        for j in range(sb_row * 3, sb_row * 3 + 3):
+          if len(self.data[i]) >= j:
+            row_list.append(self.data[i][j])
     print('data check:', self.data)
     print('should be done, here is your row list: ', row_list)
     return row_list
   
+  # next to do -- make adjustments to get_col per get_row solution
   def get_col(self, sub_block,col):
-    if len(self.data) <= 1:
+    board_row = sub_block // 3
+    if board_row == 0:
+      print('data check:', self.data)
+      print('should be done, here is your col list: ', [])
       return []
     col_list = []
-    board_row = (sub_block % 3)
     sb_row = (col // 3)
-    for i in range(board_row, board_row + 7, 3):
+    for i in range(sub_block, sub_block - board_row * 3, -3):
       if len(self.data) >= i:
         for j in range(sb_row, sb_row + 7, 3):
           if len(self.data[i]) >= j:
             col_list.append(self.data[i][j])
-            print(col_list)
+    print('data check:', self.data)
+    print('should be done, here is your col list: ', col_list)
     return col_list
 
 
@@ -44,10 +51,8 @@ class Board():
         print('running new loop, j: ', j)
         sub_block = i
         fc = j
-        row = j // 3
-        col = j % 3
-        row_list = self.get_row(sub_block, row)
-        col_list = self.get_col(sub_block, col)
+        row_list = self.get_row(sub_block, fc)
+        col_list = self.get_col(sub_block, fc)
         while len(self.data[i]) < j + 1 :
           fc = random.randrange(1,10,1) # fixed cell = random number 1-9(inclusive)
           if fc not in self.data[sub_block]: # fixed cell no in sub-block
