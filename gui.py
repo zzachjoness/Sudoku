8c8f95a28190a8f3a98fb778bc464259e1d5a923
zzachjoness <zzachjoness@gmail.com>
2023-01-27 15:55:41 -0500

    gui update

--- gui.py
+++ gui.py
@@ -14,30 +14,30 @@ class Cell():
 
 
 # Function for creating a Frame of Cells
-def mk_board(containter, cell):
+def mk_board(containter, cell, data):
     frame = ttk.Frame(containter)
     frame.columnconfigure(0, weight=1)
     frame.columnconfigure(1, weight=1)
     frame.columnconfigure(2, weight=1)
     frame.grid(column=cell % 3, row=cell//3)
     for i in range(9):
-        field = ttk.Label(frame, background='#0c3800', text=i+1, width=1, padding=10, border=1, relief='sunken')
+        field = ttk.Label(frame, background='#0c3800', text=data[i], width=1, padding=10, border=1, relief='sunken')
         field.grid(column=i % 3, row=i//3)
 
 
 # Function for creating grid of Frames
 def mk_grid(container):
+    board = gs.Board()
+    board.iterate_gen()
     for i in range(9):
-        mk_board(container, i)
+        mk_board(container, i, board.data[i])
 
 #root window
 root = tk.Tk()  # creates and application windon
 root.geometry('400x400')
 root.resizable(True, True)
 root.title('Sudoku')
-
-#board_one = gs.Board()
-#board_one.generate()
+mk_grid(root)
 
 root.mainloop()  # keeps the window visible on the screen
 
