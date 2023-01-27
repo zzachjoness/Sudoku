691c968ba71940dd3edca95552f15d6749e32eed
zzachjoness <zzachjoness@gmail.com>
2023-01-27 14:15:17 -0500

    dice & choices array add

--- generate_Sudoku.py
+++ generate_Sudoku.py
@@ -7,6 +7,8 @@ class Board():
   def __init__(self):
     self.data = []
     self.msg = 'hi'
+  def reset(self):
+    self.data = []
 
   def get_row(self, sub_block,fc):
     board_col = sub_block % 3
@@ -18,11 +20,9 @@ class Board():
       if len(self.data) >= i:
         for j in range(sb_row * 3, sb_row * 3 + 3):
           if len(self.data[i]) >= j:
-            row_list.append(self.data[i][j])
-    
+            row_list.append(self.data[i][j])  
     return row_list
   
-  # next to do -- make adjustments to get_col per get_row solution
   def get_col(self, sub_block, fc):
     board_row = sub_block // 3
     if board_row == 0:
@@ -34,26 +34,30 @@ class Board():
         for j in range(sb_col, sb_col + 7, 3):
           if len(self.data[i]) >= j:
             col_list.append(self.data[i][j])
-    print('data check:', self.data)
-    print('should be done, here is your col list: ', col_list)
     return col_list
 
-  # need better planning, get_col + get_row working properly
+  # next to do, if failure, start over
   def generate(self):
     for i in range(9):
       self.data.append([])
+      choices = [1,2,3,4,5,6,7,8,9]
+      last_len = len(choices)
       for j in range(9):
         sub_block = i
         fc = j
         row_list = self.get_row(sub_block, fc)
         col_list = self.get_col(sub_block, fc)
         while len(self.data[i]) < j + 1 :
-          fc = random.randrange(1,10,1) # fixed cell = random number 1-9(inclusive)
-          if fc not in self.data[sub_block]: # fixed cell no in sub-block
-            if fc not in row_list: # fixed cell not in row
-              if fc not in col_list: # fixed cell not in col
-                self.data[i].append(fc)
-    print(self.data)
+          dice = random.randrange(len(choices))
+          fc = choices[dice]
+          if fc not in row_list: # fixed cell not in row
+            if fc not in col_list: # fixed cell not in col
+              self.data[i].append(fc)
+              del choices[dice]
+              last_len = len(choices)
+          print(last_len)
+        
+    print('done: ',self.data)
 
   def show(self):
     print(self.msg,self.data)
