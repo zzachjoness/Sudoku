c7e58a7c96912ceb594213b60a4509cc0f219e2a
zzachjoness <zzachjoness@gmail.com>
2023-01-31 16:10:34 -0500

    resize/clean up gui

--- gui.py
+++ gui.py
@@ -12,33 +12,36 @@ colors = ['white smoke', 'light coral', 'khaki', 'pink', 'peach puff', 'lemon ch
 
 # Function for creating a Frame of Cells
 def mk_board(containter, cell, data):
-    frame = Frame(containter,width=450, height=450)
-    frame.columnconfigure(0,)
-    frame.columnconfigure(1, minsize=30)
-    frame.columnconfigure(2, minsize=30)
+    frame = Frame(containter, borderwidth=.5, relief='solid')
+    frame.columnconfigure(0, weight=1, minsize=50)
+    frame.columnconfigure(1, weight=2, minsize=50)
+    frame.columnconfigure(2, weight=1, minsize=50)
+    frame.rowconfigure(0, weight=1, minsize=50)
+    frame.rowconfigure(1, weight=1, minsize=50)
+    frame.rowconfigure(2, weight=1, minsize=50)
     frame.grid(column=cell % 3, row=cell//3)
     for i in range(9):
         number = data[i]
-        field = Label(frame, background=colors[number-1], text=number, relief='ridge', borderwidth=1) #padding=10 removed
-        field.grid(column=i % 3, row=i//3)
+        field = Label(frame, text=number, borderwidth=.5, relief='raised') #, background=colors[number-1]) #padding=10 removed
+        field.grid(column=i % 3, row=i//3, sticky='nsew')
 
 # Function for creating grid of Frames
 def mk_grid(container):
     board = gs.Board()
     board.iterate_gen()
-    board_frame = Frame(container, bg='white', width=450, height=450, borderwidth=1, relief='flat')
-    board_frame.grid(row=0, sticky='ew')
+    board_frame = Frame(container, bg='white', borderwidth=1, relief='solid')
+    board_frame.grid(row=0)
     for i in range(9):
         mk_board(board_frame, i, board.data[i])
 
 #root window
 root = Tk()  # creates and application window
 root.title('Sudoku')
-root.geometry('{}x{}'.format(600,600))
+root.geometry('{}x{}'.format(800,550))
 #root.resizable(True, True)
 
 # create all of the main containers
-top_frame = Frame(root, bg='cyan', width=550, height=50, pady=3)
+top_frame = Frame(root, bg='cyan', width=550, height=150, pady=3)
 center_frame = Frame(root, bg='lavender', width=550, height=300, pady=3)
 btm_frame = Frame(root, bg='gray2', width=550, height=50, pady=3)
 
@@ -47,15 +50,14 @@ root.grid_rowconfigure(1, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
 top_frame.grid(row=0, sticky='ew')
-center_frame.grid(row=1, sticky='nsew')
+center_frame.grid(row=1, sticky='ns')
 btm_frame.grid(row=2,stick='ew')
 
 # create widgets for the top frame
-new_board_button = Button(top_frame, bg='red', text="New Board", command=lambda: mk_grid(center_frame))
+new_board_button = Button(top_frame, bg='red', text="New Board", height=2, width=10, command=lambda: mk_grid(center_frame))
 
 # layout the widgets for the top frame
 new_board_button.grid(row=0) #test with colspan
-
 
 # create widgets for the btm frame
 close_window = Button(btm_frame, text="Close Window", command=lambda: root.quit())
